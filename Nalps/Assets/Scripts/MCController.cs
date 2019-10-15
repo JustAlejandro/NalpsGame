0221568ddd234b9bc4eefe9138d611af2442efd3
Angel R. Argandona Vite <angel.argandonavite@gmail.com>
2019-10-14 21:59:39 -0500

    Implemented grid-movement. See TestScene.

---
+++ Nalps/Assets/Scripts/MCController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCController : MonoBehaviour
+{
+    private float speed = 3.0f;
+    Transform transform;
+    Vector3 position;
+    //Animator animator;
+    //Vector2 lookDirection = new Vector2(1, 0);
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        //animator = GetComponent<Animator>();
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal != 0.0f && transform.position == position)
+        {
+            Vector3 move = new Vector3(horizontal, 0, 0);
+            move.Normalize();
+            position += move;
+        }
+        else if (vertical != 0.0f && transform.position == position)
+        {
+            Vector3 move = new Vector3(0, vertical, 0);
+            move.Normalize();
+            position += move;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+    }
+
+}
