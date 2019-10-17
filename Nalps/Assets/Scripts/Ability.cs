f248a30eed76e72804d06a70fe8d151d15a8f5e9
JustAlejandro <32211773+JustAlejandro@users.noreply.github.com>
2019-10-16 22:43:32 -0500

    Battle Framework
    Setup both the Nalp and Ability Default types

---
+++ Nalps/Assets/Scripts/Ability.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+//BattleData holds the response to the attack
+public class BattleData {
+    //Did move hit
+    public bool hit;
+    //Did move crit?
+    public bool crit;
+    //Was this a favorable type matchup?
+    public float multiplier;
+
+    public int damage;
+
+    //Type and name of the Move(for UI use)
+    public int type;
+    public string name;
+
+    public BattleData(bool h = false, bool c = false, int d = 0, int t = (int)Types.Typeless, string n = "NAMELESS MOVE") {
+        hit = h;
+        crit = c;
+        damage = d;
+        type = t;
+        name = n;
+    }
+}
+
+//All attacks and other stuff in handled here, with the different types of moves being subclasses
+//Not the ideal setup, but not terrible either
+public class Ability
+{
+    protected int maxUse;
+    protected int curUse;
+    protected int damage;
+    protected int type;
+    protected float accuracy;
+    protected float critChance;
+    protected string name;
+
+    public Ability() {
+        maxUse = 5;
+        curUse = 5;
+        damage = 5;
+        type = (int)Types.Typeless;
+        accuracy = 1.0f;
+        critChance = 0.0f;
+        name = "DEFAULT MOVE";
+    }
+
+    //Return the BattleData to be read by the enemy
+    //Input: Nalp to modify the move if the user's stats influence its power
+    public virtual BattleData use(Nalp user) {
+        curUse--;
+        return new BattleData(Hit(), Crit(), damage, type, name);
+    }
+
+    //Check if a move hit/crit
+    protected bool Hit() {
+        return Random.value < accuracy;
+    }
+    protected bool Crit() {
+        return Random.value < critChance;
+    }
+
+    //Getters for Ability field stuff
+    public int CurUse { get => curUse; }
+    public int MaxUse { get => maxUse; }
+    public int Damage { get => damage; }
+    public int Type { get => type; }
+    public float Accuracy { get => accuracy; }
+    public float CritChance { get => critChance; }
+    public string Name { get => name; }
+}
+
+public class Tackle : Ability {
+    public Tackle() {
+        maxUse = 10;
+        curUse = 10;
+        damage = 10;
+        type = (int)Types.Typeless;
+        accuracy = 1.0f;
+        critChance = 0.1f;
+        name = "Tackle";
+    }
+}
+
+public class FireBall : Ability {
+    public FireBall() {
+        maxUse = 5;
+        curUse = 5;
+        damage = 20;
+        type = (int)Types.Fire;
+        accuracy = 0.8f;
+        critChance = 0.05f;
+        name = "Fire Ball";
+    }
+
+    public override BattleData use(Nalp user) {
+        int d = damage + user.Strength;
+        curUse--;
+        return new BattleData(Hit(), Crit(), d, type, name);
+    }
+}
---
+++ Nalps/Assets/Scripts/Nalp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Types {
+    Typeless,
+    Fire,
+    Water,
+    Grass
+}
+
+[System.Serializable]
+//This is the class that will be the basis for all Nalps, player and enemy alike
+public class Nalp : MonoBehaviour
+{
+    protected int hp;
+    protected int xp;
+    protected int level;
+    protected int type;
+    protected int speed;
+    protected int strength;
+    protected int resistance;
+    protected string nalpname;
+    protected List<Ability> moveList;
+    protected Nalp enemy;
+
+    public int Hp { get => hp; set => hp = value; }
+    public int Xp { get => xp; set => xp = value; }
+    public int Level { get => level; set => level = value; }
+    public int Type { get => type; set => type = value; }
+    public int Speed { get => speed; set => speed = value; }
+    public int Strength { get => strength; set => strength = value; }
+    public int Resistance { get => resistance; set => resistance = value; }
+    public List<Ability> MoveList { get => moveList; set => moveList = value; }
+    public string Name { get => nalpname; }
+
+    //Default Nalp Constructor
+    public Nalp() {
+        Hp = 10;
+        Xp = 0;
+        Level = 1;
+        MoveList = new List<Ability>();
+        //Default Nalp only knows tackle
+        MoveList.Add(new Tackle());
+        nalpname = "Default Nalp";
+        enemy = null;
+    }
+
+    //Give the nalp a target for combat
+    public void enterCombat(Nalp e) {
+        enemy = e;
+    }
+}
