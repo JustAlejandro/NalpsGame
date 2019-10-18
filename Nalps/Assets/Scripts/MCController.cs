343319952439605f97a1aaadb7063451a569d36b
Angel R. Argandona Vite <angel.argandonavite@gmail.com>
2019-10-18 00:24:05 -0500

    Added collisions

--- Nalps/Assets/Scripts/MCController.cs
+++ Nalps/Assets/Scripts/MCController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MCController : MonoBehaviour
 {
     private float speed = 3.0f;
-    Transform transform;
     Vector3 position;
     //Animator animator;
     //Vector2 lookDirection = new Vector2(1, 0);
@@ -14,7 +13,6 @@ public class MCController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform = GetComponent<Transform>();
         //animator = GetComponent<Animator>();
         position = transform.position;
     }
@@ -24,17 +22,28 @@ public class MCController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
         if (horizontal != 0.0f && transform.position == position)
         {
-            Vector3 move = new Vector3(horizontal, 0, 0);
-            move.Normalize();
-            position += move;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(horizontal, 0),1.0f, LayerMask.GetMask("Static Objects"));
+            if (!hit)
+            {
+                Debug.Log("Bye");
+                Vector3 move = new Vector3(horizontal, 0, 0);
+                move.Normalize();
+                position += move;
+            }
         }
         else if (vertical != 0.0f && transform.position == position)
         {
-            Vector3 move = new Vector3(0, vertical, 0);
-            move.Normalize();
-            position += move;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, vertical),1.0f, LayerMask.GetMask("Static Objects"));
+            if (!hit)
+            {
+                Debug.Log("HI");
+                Vector3 move = new Vector3(0, vertical, 0);
+                move.Normalize();
+                position += move;
+            }  
         }
         transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
     }
