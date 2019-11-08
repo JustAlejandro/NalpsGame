a02a443f71d3dea311b32505a3730e5b5d0f3bbc
Angel R. Argandona Vite <angel.argandonavite@gmail.com>
2019-11-07 21:07:02 -0600

    random stuff

--- Nalps/Assets/Scripts/MCController.cs
+++ Nalps/Assets/Scripts/MCController.cs
@@ -10,7 +10,7 @@ public class MCController : MonoBehaviour
     public bool inGrass = false;
     private bool checkEncounter = true;
     //Animator animator;
-    //Vector2 lookDirection = new Vector2(1, 0);
+    Vector2 lookDirection = new Vector2(1, 0);
 
     
     // Start is called before the first frame update
@@ -26,7 +26,10 @@ public class MCController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        lookDirection.Set(horizontal, vertical);
+        lookDirection.Normalize();
 
+        // This if statement trigger random encounters when the player is inside grass
         if (transform.position == position & inGrass & checkEncounter)
         {
             checkEncounter = false;
@@ -36,11 +39,11 @@ public class MCController : MonoBehaviour
             {
                 Debug.Log("Fight!");
                 GameObject.FindGameObjectsWithTag("PlayerData")[0].GetComponent<PlayerScript>().position = position;
-
                 SceneManager.LoadScene("BattleScene");
             }
         }
 
+        // The following code moves the MC if a collision is not detected by the raycast
         if (horizontal != 0.0f && transform.position == position)
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(horizontal, 0),1.0f, LayerMask.GetMask("Static Objects"));
@@ -64,6 +67,16 @@ public class MCController : MonoBehaviour
             }  
         }
         transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+
+        // The following is used to pick up items
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            RaycastHit2D findItem = Physics2D.Raycast(transform.position, lookDirection, 1.0f);
+            if (findItem.collider.GetComponent<Item>() != null)
+            {
+                Debug.Log("Found Item");
+            }
+        }
     }
 
 }
